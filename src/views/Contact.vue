097b834ff4b3c0063dde383de55124311dcba37f
yhdchoi <yhdchoi@gmail.com>
2021-07-05 22:19:50 +0900

    Views
    All views are set to be filled in with contents.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,104 @@
 <template>
-    <div class="h-screen bg-black text-white p-10">
-        <h2>Contact</h2>
+  <div class="h-screen bg-black text-white p-10">
+    <div class="w-full text-center">
+      <h1 class="font-bold text-4xl text-white  hover:text-green-500 my-8">
+        Contact 
+      </h1>
     </div>
+
+    <form class="w-full max-w-lg m-auto" >
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+            for="grid-password"
+          >
+            Title
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            name="title"
+            type="text"
+            placeholder="Required"
+          />
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+            for="grid-password"
+            
+          >
+            E-mail
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            name="email"
+            type="email"
+            placeholder="Required"
+          />
+          
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+            for="grid-password"
+          >
+            Message
+          </label>
+          <textarea
+            class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+            name="message"
+            placeholder="Your message here"
+          ></textarea>
+        </div>
+      </div>
+      <div class="md:flex md:items-center">
+        <div class="md:w-1/3">
+          <button
+            class="bg-gray-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+            value="Send"
+          >
+            Send
+          </button>
+        </div>
+        <div class="md:w-2/3"></div>
+      </div>
+    </form>
+
+    <!-- <div class="flex flex-wrap -mx-3 mb-6 ">
+      <form name="contact-form" @submit.prevent="sendEmail">
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message"></textarea>
+        <input type="submit" value="Send" />
+      </form>
+    </div> -->
+  </div>
 </template>
+
+
+<script>
+  // import emailjs from 'emailjs-com';
+
+  // export default {
+  //   methods: {
+  //     sendEmail: (e) => {
+  //       emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+  //         .then((result) => {
+  //             console.log('SUCCESS!', result.status, result.text);
+  //         }, (error) => {
+  //             console.log('FAILED...', error);
+  //         });
+  //     }
+  //   }
+  // }
+  //
+</script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,125 @@
 <template>
-  <div class="h-screen bg-black text-white p-10">
-    <h2>DEcrypt</h2>
+  <div class="h-screen bg-black p-10">
+    <div class="w-full text-center">
+      <h1 class="font-bold text-5xl text-white  hover:text-green-500 my-6">
+        DEcrypt
+      </h1>
+      <p></p>
+    </div>
+
+    <div class="flex-grow flex justify-center items-center mt-6">
+      <div class="mx-auto px-4 sm:px-8 py-2 text-center">
+        <div class="text-center max-w-lg mx-auto">
+          <div class="text-xl text-white my-6">
+            <p>Designed to be anonymous...</p>
+          </div>
+        </div>
+
+        <div class="grid grid-cols-6 gap-4 items-start mt-8 mx-auto px-8">
+          <div
+            class="col-span-6 sm:col-span-6 md:col-span-3 lg:col-span-2 xl:col-span-2"
+          >
+            <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
+              <div class="mx-auto h-40 bg-gray-500 rounded-md p-4">
+                <h1 class="font-bold text-xl text-white">FRONTEND</h1>
+                <div class="flex justify-center items-center mt-8">
+                  <img
+                    class="mr-8"
+                    src="https://img.icons8.com/color/480/000000/vue-js.png"
+                    width="50px"
+                    height="50px"
+                  />
+                  <img
+                    class="mr-8"
+                    src="https://img.icons8.com/color/48/000000/npm.png"
+                    width="50px"
+                    height="50px"
+                  />
+                  <img
+                    src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+                    width="50px"
+                    height="50px"
+                  />
+                </div>
+              </div>
+              <div
+                class="h-4 bg-gray-200 w-40 mt-8 block mx-auto rounded-sm"
+              ></div>
+              <div
+                class="h-2 bg-gray-200 w-64 mt-2 block mx-auto rounded-sm"
+              ></div>
+            </div>
+          </div>
+
+          <div
+            class="col-span-6 sm:col-span-6 md:col-span-3 lg:col-span-2 xl:col-span-2"
+          >
+            <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
+              <div class="mx-auto h-40 bg-gray-500 rounded-md p-4">
+                <h1 class="font-bold text-xl text-white">BACKEND</h1>
+                <div class="flex justify-center items-center mt-8">
+                  <img
+                    class="mr-8"
+                    src="https://img.icons8.com/color/48/000000/spring-logo.png"
+                    width="50px"
+                    height="50px"
+                  />
+                  <img
+                    class="mr-8"
+                    src="https://www.vectorlogo.zone/logos/hibernate/hibernate-icon.svg"
+                    width="50px"
+                    height="50px"
+                  />
+                  <img
+                    src="https://img.icons8.com/color/48/000000/maria-db.png"
+                    width="50px"
+                    height="50px"
+                  />
+                </div>
+              </div>
+              <div
+                class="h-4 bg-gray-200 w-40 mt-8 block mx-auto rounded-sm"
+              ></div>
+              <div
+                class="h-2 bg-gray-200 w-64 mt-2 block mx-auto rounded-sm"
+              ></div>
+            </div>
+          </div>
+
+          <div
+            class="col-span-6 sm:col-span-6 md:col-span-6 lg:col-span-2 xl:col-span-2"
+          >
+            <div class="bg-white shadow-lg rounded-lg px-4 py-6 mx-4 my-4">
+              <div class="mx-auto h-40 bg-gray-500 rounded-md p-4">
+                <h1 class="font-bold text-xl text-white">SERVER</h1>
+                <div class="flex justify-center items-center mt-8">
+                  <img
+                    class="mr-6"
+                    src="https://img.icons8.com/nolan/64/github.png"
+                    width="50px"
+                    height="50px"
+                  />
+                  <img
+                    src="https://img.icons8.com/color/48/000000/amazon-web-services.png"
+                    width="50px"
+                    height="50px"
+                  />
+                </div>
+              </div>
+              <div
+                class="h-4 bg-gray-200 w-40 mt-8 block mx-auto rounded-sm"
+              ></div>
+              <div
+                class="h-2 bg-gray-200 w-64 mt-2 block mx-auto rounded-sm"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full text-center my-6">
+      <p>The </p>
+    </div>
   </div>
 </template>
