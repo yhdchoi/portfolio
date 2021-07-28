ebc95d6d87293675ce25e884a8ec29fab87795fd
yhdchoi <yhdchoi@gmail.com>
2021-07-28 22:39:54 +0900

    Korean Translation

--- src/views/JBlog.vue
+++ src/views/JBlog.vue
@@ -50,6 +50,14 @@
       </div>
     </div>
     <br />
+
+    <h5 class="text-3xl text-blue-300 font-bold mb-2">Description:</h5>
+    <p class="text-base text-white mb-10 w-3/5 m-auto">
+      The project showcases the overall processes of Spring and its components.
+      The database is managed through JPA with MariaDB and the security is
+      managed with Spring Security with OAuth2.0.
+    </p>
+    <br />
     <!-- Login&Signup -->
     <div class="max-w-lg mx-auto mb-4">
       <div
@@ -61,42 +69,55 @@
       </div>
     </div>
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      The login process is built using the Spring Security and OAuth2.0.
+      The login page is created using JSP/JSTL where the logged in and logged
+      out users are differnetiated. And only logged in users are aloowed to
+      write any articles or comments.
     </p>
+
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      로그인 페이지는 로그인 유져와 로그아웃된 유져들을 구별하며 로그인 된
+      유져들만 글을 작성가능.
+    </p>
+
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/MenuJSTL.png" />
-    <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      I have created login page with JSP/JSTL where the navigation bar is coded
-      to differentiate anonymous users and registered users.
-    </p>
-    <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/nav1.png" />
-    <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/nav2.png" />
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/login.png" />
-    <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/loginkakao.png" />
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      For this example Form Login is used and CSFR is disabled for the
-      development purpose. The authorize request is set with home, signup and
-      login pages only so that anyone without proper authority can not access
-      the full site.
+      Form Login is used and CSFR is disabled for the development purpose. The
+      authorize request is set with home, signup and login pages only so that
+      anyone without proper authority can not access the full site.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      개발을위해 CSFR을 disable 했으며 보안을 위해 로그인이 안된 유져들은 로그인
+      페이지만 허용하였음.
     </p>
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/loginconfig.png" />
+
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
       The login with Kakao requires to request for a token from kakao api. And
       we can use this token to request for user information. In this project I
-      have only have aquired for user ID and email to create a profile.
+      have aquired only for user ID and email to create a profile.
     </p>
-    <img
-      class="w-3/5 mb-10 m-auto"
-      src="../assets/jblog/loginconfigkakao.png"
-    />
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      Kakao 로그인은 Api로부터 토큰을 발급받아 이를 이용하여 유져의 정보를
+      가져옴.
+    </p>
+    <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/loginconfigkakao.png" />
+    <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/loginkakao.png" />
+
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
       The passwords are vaildated through javascript where making sure that the
       both passwords are identical.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      Javascript를 이용하여 비밀번호 중복 체크를 하였고 Ajax로 새로운 유져
+      가입을 구현함.
     </p>
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/signup.png" />
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/joinjs.png" />
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/signupconfpass.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/signupsucc.png" />
     <br />
+
     <!-- Database -->
     <div class="max-w-lg mx-auto mb-4">
       <div class="text-3xl font-bold text-green-300 hover:text-green-500 mb-2">
@@ -106,8 +127,10 @@
       </div>
     </div>
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      MariaDB is chosen as the DBMS used for this project. The belows are
-      objects and entities.
+      MariaDB is chosen as the DBMS used for this project.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      MariadDB를 이용하여 Database를 구현함.
     </p>
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/entityoauth.png" />
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/entitykakao.png" />
@@ -115,9 +138,14 @@
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/entityboard.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/entitycomment.png" />
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      With JPA you can create query with method names and I was able to create custom methods.
+      With JPA you can create query with method names and I was able to create
+      custom methods.
     </p>
-<img class="w-3/5 mb-6 m-auto" src="../assets/jblog/repouser.png" />
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      JPA의 커스텀 query를 이용하여 필요한 query와 Pageable을 이용하여
+      검색기능과 Pagination을 구현함.
+    </p>
+    <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/repouser.png" />
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/repoboard.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/repocomment.png" />
 
@@ -134,21 +162,34 @@
     </div>
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
       After logging in, you are redirected to the main page where a list of
-      articles are displayed.
+      articles are displayed. To indicate the types, ENUMs are used and
+      uni-directional and bi-directional relationship is used to establish the
+      relationsips between the tables.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      로그인 후 메인페이지로 이동하며 게시판의 모든 글을 볼수있음. ENUM을
+      이용하여 role과 같은 type들을 구현하였음. 단방향과 양방향 관계를 이용하여
+      테이블 간의 관계를 형성하였음.
     </p>
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/mainlist.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/mainpagenation.png" />
-    <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      The search bar, list and pagination are created using JSTL and Spring
-      Boot.
-    </p>
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/boardsearch.png" />
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/boardlist.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/boardpagination.png" />
+
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      The search runs in both the title and contents of the articles. The
-      procedure takes with a request carring keywords for the search then JPA
-      with custom query run the search and responses the results back.
+      One of the common issues when using JPA is the infinite recursion. In
+      order to solve this problem, the JsonIgnorProperties annotation is used.
+      <br />
+      <br />
+      When removing datas, such as articles, we need to remove the comments that
+      are related to each article. The CascadeType is used to remove all the
+      datas that are forming relationships with.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      양방향 관계에서 흔히 나타나는 무한반복 해결책으로 JsonIgnorProperties
+      annotation을 사용하였음. CascadeType을 이용하여 연관되어 있는 데이터를
+      동시에 삭제가능하게 구현함.
     </p>
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/mainsearch.png" />
     <img
@@ -171,14 +212,16 @@
       </div>
     </div>
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      The user information is displayed and can be edited on My Profile page.
+      The user information is displayed and can be edited on My Profile page. It
+      is processed through Javascript and Ajax to the controller and service
+      where the changes are validated and saved.
     </p>
-    <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/profilepage.png" />
-
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      The user information processed through javascript and ajax to the
-      controller and service where the changes are validated and saved.
+      사용자 정보는 Profile 페이지에서 수정가능함. Javascript와 Ajax를 활용하여
+      수정돤 정보를 확인하고 POST함.
     </p>
+
+    <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/profilepage.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/profileupdatejs.png" />
     <img
       class="w-3/5 mb-10 m-auto"
@@ -194,32 +237,38 @@
       </div>
     </div>
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      In order to write a new article, click on the 'Write' button on the
-      navigation bar.
+      The Write button will take you to the page where you can composed a new
+      article. After posting the article you will be redierected to the main
+      list page where you can check your new article.
     </p>
-    <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/writenew.png" />
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      Here you can write a new article as shown in the example. You can add
-      files and format it as you wish.
+      Write 버튼은 게시물 작성 페이지로 이동함. 그리고 게시물 저장후 다시 목록
+      페이지로 이동됨.
     </p>
-    <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/writenew2.png" />
-    <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      By clicking the SAVE button, you are automatically redirected to the main
-      page where your article is posted.
-    </p>
+    <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/writenew.png" />
+    <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/writenew2.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/writenewlist.png" />
+
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
       By clicking the 'Read' button on each card, you are directed to the detail
       of your article where you can edit and delete it.
     </p>
-    <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/writenewdetail.png" />
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      Below are the javascript, ajax and Spring for the above processes.
+      Read 버튼으로 자세히보기 페이지로 이동하여 게시물 수정 및 삭제를 할수있음.
+    </p>
+    <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/writenewdetail.png" />
+
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      Below are the Javascript, Ajax and Spring for the above processes.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      Javascript, Ajax and Spring를 활용하여 구현됨.
     </p>
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/boardcontroljs.png" />
     <img class="w-3/5 mb-6 m-auto" src="../assets/jblog/boardcontrol.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/boardservice.png" />
     <br />
+
     <!-- Comment -->
     <div class="font-bold max-w-lg mx-auto">
       <div class="text-3xl text-green-300 hover:text-green-500 mb-4">
@@ -230,6 +279,9 @@
     </div>
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
       The comments can be added at the bottom of the Read page of each articles.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      댓글은 게시물 하단쪽에서 작성 가능.
     </p>
     <img class="w-3/5 mb-4 m-auto" src="../assets/jblog/writenewcomment.png" />
     <img class="w-3/5 mb-4 m-auto" src="../assets/jblog/writenewcomment2.png" />
@@ -243,7 +295,10 @@
       src="../assets/jblog/writenewcommentdelete.png"
     />
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
-      Above processes are completed through ajax to the controller and service.
+      Above processes are completed through Ajax to the controller and service.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      Ajax를 활용하여 게시물 작성 및 편집을 구현
     </p>
     <img class="w-3/5 mb-4 m-auto" src="../assets/jblog/commentcontroljs.png" />
     <img class="w-3/5 mb-4 m-auto" src="../assets/jblog/commentcontrol.png" />
@@ -259,6 +314,9 @@
     </div>
     <p class="text-lg text-white mb-8 w-3/5 m-auto">
       By clicking Logout button, you are redirected to the Main public page.
+    </p>
+    <p class="text-lg text-white mb-8 w-3/5 m-auto">
+      Logout을 하면 다시 메인 페이지로 이동.
     </p>
     <img class="w-3/5 mb-4 m-auto" src="../assets/jblog/mainpublic.png" />
     <img class="w-3/5 mb-10 m-auto" src="../assets/jblog/logoutconfig.png" />
