e294f9bd61850d491b0124337eca0895662b7940
yhdchoi <yhdchoi@gmail.com>
2021-11-28 21:53:20 +0900

    Balmuda Mockup

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -250,6 +250,84 @@
 
       <br />
 
+      <!-- Mockup -->
+      <div class="w-3/5 mx-auto my-6">
+        <a
+          class="animate-pulse text-purple-200 flex items-center shadow-none justify-center"
+          href="https://github.com/yhdchoi/mockup"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            fill="purple"
+            class="w-8 h-8 mb-11 mr-3"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+            />
+          </svg>
+          <p class="text-lg text-red-500 mb-10">
+            Balmuda Mockup
+          </p>
+        </a>
+        <div
+          class="w-full shadow-2xl subpixel-antialiased rounded h-72 bg-gray-500 border-black m-auto overflow-auto"
+        >
+          <div
+            class="flex items-center h-6 rounded-t bg-gray-100 border-b border-gray-500 text-center text-black"
+            id="headerTerminal"
+          >
+            <div
+              class="flex ml-2 items-center text-center border-red-900 bg-red-500 shadow-inner rounded-full w-3 h-3"
+              id="closebtn"
+            ></div>
+            <div
+              class="ml-2 border-yellow-900 bg-yellow-500 shadow-inner rounded-full w-3 h-3"
+              id="minbtn"
+            ></div>
+            <div
+              class="ml-2 border-green-900 bg-green-500 shadow-inner rounded-full w-3 h-3"
+              id="maxbtn"
+            ></div>
+            <div class="mx-auto pr-16" id="terminaltitle">
+              <p class="text-center text-base text-red-500">
+                Mockup
+              </p>
+            </div>
+          </div>
+          <div class="pl-1 pt-1 font-mono text-base bg-gray-500" id="console">
+            <p class="pb-1 px-1  text-green-400">
+              daniel$
+              <a
+                href="http://yhdchoi.dothome.co.kr/balmuda_mockup/index.html"
+                class="animate-pulse border-b-2 text-pink-700 hover:text-indigo-900 hover:border-indigo-700 ml-2"
+                target="_blank"
+                rel="noopener noreferrer"
+                >Visit Balmuda Mockup</a
+              >
+              <br />
+              <br />
+              daniel$ project description; <br />
+              The study of Html & CSS.
+              <br />
+              <br />
+              daniel$ print tools used;<br />
+              Html, CSS, Filezilla;
+              <br />
+              <br />
+              daniel$
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- Mockup Ends -->
+
+      <br />
+
       <!-- React_BB -->
       <!-- <div class="w-3/5 mx-auto my-6">
       <a
